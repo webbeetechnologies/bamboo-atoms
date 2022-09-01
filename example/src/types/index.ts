86c041a2635b67aa8b0e89dfd978e9abbb39c620
Thet Aung <thetaung.dev@gmail.com>
2022-09-02 06:07:10 +0630

    updating CustomTheme interface to account for custom component styles

--- example/src/types/index.ts
+++ example/src/types/index.ts
@@ -1,5 +1,9 @@
-import type { Theme } from 'bamboo-shoots';
+import type { ComponentStyles, Theme, StyleWithTheme } from 'bamboo-shoots';
+import type { ViewStyle } from 'react-native';
 
 export interface CustomTheme extends Theme {
     primaryColor: string;
+    componentStyles: ComponentStyles & {
+        CustomButton: StyleWithTheme<ViewStyle>;
+    };
 }
