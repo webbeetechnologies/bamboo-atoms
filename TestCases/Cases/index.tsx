b789e7d328e759a4826342891830cb9ec1e63387
Shashank Agarwal <shashank1010@gmail.com>
2022-09-09 15:32:18 +0300

    Text test cases

--- TestCases/Cases/index.tsx
+++ TestCases/Cases/index.tsx
@@ -1,24 +1,49 @@
-import { View } from "react-native";
-import { Card, Divider } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Card, Headline } from "react-native-paper";
+import NestedText from "./NestedText";
 import TopLevelContext from "./TopLevelContext";
 import TopLevelStyles from "./TopLevelStyles";
 
+
+const style = StyleSheet.create({
+    wrap: { gap: 15, padding: 15 } as any,
+    cardTitle: {
+        marginHorizontal: -2,
+        marginTop: -2,
+        marginBottom: 16,
+        backgroundColor: "#d5d5d5"
+    }
+})
+
+const CardHeader: typeof Headline = (props) => {
+    return <Card.Title style={style.cardTitle} title={<Headline {...props}></Headline>} />
+}
+
 export default () => {
     return (
-        <View style={{ gap: 15, padding: 15 } as any}>
+        <View style={style.wrap}>
             <Card>
+                <CardHeader children="Nested Text" />
                 <Card.Content>
-                <TopLevelContext />
+                    <NestedText />
                 </Card.Content>
             </Card>
+
+            
             <Card>
-            <Card.Content>
-
-            <TopLevelStyles />
-            </Card.Content>
-
+                <CardHeader children="Top Level Components" />
+                <Card.Content>
+                    <TopLevelContext />
+                </Card.Content>
             </Card>
 
+
+            <Card>
+                <CardHeader children="Top Level Styles" />
+                <Card.Content>
+                    <TopLevelStyles />
+                </Card.Content>
+            </Card>
         </View>
     )
 }
