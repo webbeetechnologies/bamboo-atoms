174d779745f8c6ab9170182472ddc64bc3070fa1
Thet Aung <thetaung.dev@gmail.com>
2022-10-13 11:00:16 +0630

    fixed ts error

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -5,7 +5,7 @@ import { ITheme, ThemeContext } from '../core/theme';
 const useTheme = <T extends ITheme>() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { extractTheme, ...theme } = useContext(ThemeContext);
-    return theme as T;
+    return theme as unknown as T;
 };
 
 export default useTheme;
